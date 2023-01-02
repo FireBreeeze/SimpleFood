c9de65a699480373f543300b06c41b1971e61cf3
FireBreeeze <amaslo111@gmail.com>
2023-01-02 19:48:05 +0000

    added restaurant-card

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,13 @@
 $(function(){
 
+  $('.reviews__slider').slick({
+    dots: true,
+    arrow: true,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
+
 });
 
 var mixer = mixitup('.categories__content');
