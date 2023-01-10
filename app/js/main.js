69ca0242df54e13e8d6ee4842802de07c10442a2
FireBreeeze <amaslo111@gmail.com>
2023-01-10 00:34:44 +0000

    fixed dz13

--- app/js/main.js
+++ app/js/main.js
@@ -3,12 +3,13 @@ $(function(){
   $('.reviews__slider').slick({
     dots: true,
     arrow: true,
-    fade: true,
+    fade: false,
     autoplay: true,
     autoplaySpeed: 3000,
+    infinite: false,
 
-    prevArrow: '<div class="slick-arrow slick-prev"><svg class="icon-chevron"><use xlink:href="images/sprite.svg#icon-chevron" viewBox="0 0 11 18"></use></svg></div>',
-    nextArrow: '<div class="slick-arrow slick-next"><svg class="icon-chevron"><use xlink:href="images/sprite.svg#icon-chevron" viewBox="0 0 11 18"></use></svg></div>',
+    prevArrow: '<div class="reviews-arrow reviews-arrow__prev"><svg class="icon-chevron"><use xlink:href="images/sprite.svg#icon-chevron" viewBox="0 0 11 18"></use></svg></div>',
+    nextArrow: '<div class="reviews-arrow reviews-arrow__next"><svg class="icon-chevron"><use xlink:href="images/sprite.svg#icon-chevron" viewBox="0 0 11 18"></use></svg></div>',
   });
 
   $('div').on('click', function() {
