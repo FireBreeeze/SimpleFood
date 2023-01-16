4ad47eeaebf3305cd984d54316648fcab11a14ed
FireBreeeze <amaslo111@gmail.com>
2023-01-16 07:20:47 +0000

    added start dz15-catalog

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,29 @@
 $(function(){
 
+  
+  $('.filter-price__input').ionRangeSlider({
+     type: "double",
+     prefix: "₴",
+    
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },   
+  });
+  
+  // $('.mobil-burger').add('burger--active');
+  // $('.mobil-burger--active').remove('burger');
+
+  $('input, select').styler();
+
   $('.reviews__slider').slick({
     dots: true,
     arrow: true,
-    fade: false,
+    // fade: false,
     // autoplay: true,
     autoplaySpeed: 3000,
     infinite: false,
@@ -12,7 +32,6 @@ $(function(){
         breakpoint: 992,
         settings: {
           dots: false,
-          infinite: true,
         }
       }
     ],
@@ -21,28 +40,26 @@ $(function(){
   }),
 
 
-  $('.restaurants__list').slick({
-    dots: true,
-    arrows: false,
-    fade: true,
-    // autoplay: true,
-    autoplaySpeed: 3000,
-    infinite: true,
-    cssEase: 'linear',
-    centerMode: true
-  });
+  // $('.restaurants__list').slick({
+  //   dots: true,
+  //   arrows: false,
+  //   fade: true,
+  //   // autoplay: true,
+  //   autoplaySpeed: 3000,
+  //   infinite: true,
+  //   cssEase: 'linear',
+  //   centerMode: true
+  // });
 
   $(window).on('load resize', function () {
-  if ($(window).width() < 768) {
+  if ($(window).width() < 620) {
    $('.restaurants__list').slick({
-    centerMode: true,
     prevArrow: false,
     nextArrow: false,
     dots: true,
-    infinite: true,
-    // speed: 100,
-    autoplay: true,
-    // slidesToShow: 1
+    // autoplay: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
    });
   } else {
     $(".restaurants__list.slick-initialized").slick("unslick");
@@ -52,23 +69,33 @@ $(function(){
 });
 
 document.addEventListener('DOMContentLoaded', () => {
-
   //Mobile Menu
   const burger = document.querySelector('.burger'); //наша кнопка
-  const mobileMenu = document.querySelector('.menu'); //мобильное меню
+  const mobileMenu = document.querySelector('.mobil-menu'); //мобильное меню
   const bodyLock = document.querySelector('body'); //ищем как селектор ТЕГА
 
   burger.addEventListener('click', () => {
-    mobileMenu.classList.toggle('menu--active'); //когда меню открыто
-    if (mobileMenu.classList.contains('menu--active'))  { //Проверяем, есть ли у меню активный класс
-      burger.classList.add('burger--active'); //Когда открыто, иконка становится крестиком
+    mobileMenu.classList.toggle('mobil-menu--active'); //когда меню открыто
+    if (mobileMenu.classList.contains('mobil-menu--active'))  { //Проверяем, есть ли у меню активный класс
+      // burger.classList.add('burger--active'); //Когда открыто, иконка становится крестиком
       bodyLock.classList.add('lock'); //Блокируем скролл при открытом меню
     }
     else { //Когда нету активного класса у меню
       burger.classList.remove('burger--active'); //Возвращает в исходное состояние
       bodyLock.classList.remove('lock'); //Разрешаем скроллить
+      mobilMenu.classList.remove('mobil-menu'); //Возвращает в исходное состояние
     }
+
+    document.addEventListener('click', function (e) {
+    if (e.target !== burger && e.target !== mobileMenu) {
+      bodyLock.classList.remove('lock');
+      mobilMenu.classList.remove('mobil-menu');  //НЕ РАБОТАЕТ
+      burger.classList.remove('burger--active');
+      }
+    });
+    
   });
+
 });
 
 var mixer = mixitup('.categories__content');
